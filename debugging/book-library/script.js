ba8fc0446ccabb04b511a151fed8b503302f4027
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-04-20 03:19:09 +0200

    Validate input before form is submitted

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -25,8 +25,10 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const check = document.getElementById("check");
 
-const submitBtn = document.querySelector('input[type="submit"]');
-submitBtn.addEventListener("click", submit);
+const submitForm = document.getElementById("bookForm");
+submitForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
