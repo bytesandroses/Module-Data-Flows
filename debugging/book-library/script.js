c4cd9b4d112902cef545f58abadd99928576092c
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-04-20 03:36:02 +0200

    Refactor: remove duplicate forms input validation

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -35,30 +35,6 @@ submitForm.addEventListener("submit", function (e) {
   render();
 });
 
-//check the right input from forms and if its ok -> add the new book (object in array)
-//via Book function and start render function
-function submit() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
-    alert("Please fill all fields!");
-    return false;
-  } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
-    myLibrary.push(book);
-
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    check.checked = false;
-
-    render();
-  }
-}
-
 function Book(title, author, pages, check) {
   this.title = title;
   this.author = author;
